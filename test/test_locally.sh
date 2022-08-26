fda30ed63e48bbc232bf176fdd47f8c6fd5d20d7
Anger Jérémy <angerj.dev@gmail.com>
2022-08-26 16:12:29 +0200

    move test/ to .ipol/test

--- test/test_locally.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-docker build -f .ipol/Dockerfile -t ipol-demo .
-
-# this is roughtly equivalent to what happens on the system during the execution
-sigma=25.3
-docker run -v $(pwd)/test:/workdir/exec --workdir /workdir/exec --user $(id -u):$(id -g) --env sigma="$sigma" --rm ipol-demo bash -c 'python $bin/main.py'
